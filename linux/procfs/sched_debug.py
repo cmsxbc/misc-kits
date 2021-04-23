ca724178a195dc1136022ff549bcabac34c4a093
cmsxbc <i@cmsis.me>
2021-04-23 21:53:46 +0800

    add tools for get some info from /proc/sched_debug

---
+++ linux/procfs/sched_debug.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import time
+import re
+
+
+SD_PATH = '/proc/sched_debug'
+
+assert os.uname().sysname == 'Linux', "linux only !!!"
+assert os.path.exists(SD_PATH), f"{SD_PATH} does not exist !!! Kernel should be built with CONFIG_SCHED_DEBUG=y"
+
+
+def get_loadavg_param():
+    with open(SD_PATH) as f:
+        in_cpu_info = False
+        nr_running = 0
+        nr_uninterruptible = 0
+        for line in f.readlines():
+            if in_cpu_info and line.startswith('  .nr_running'):
+                nr_running += int(line.split(':')[1].strip())
+            elif line.startswith('cpu#'):
+                in_cpu_info = True
+            elif line.startswith('  .nr_uninterruptible'):
+                nr_uninterruptible += int(line.split(':')[1].strip())
+            elif in_cpu_info and line[0] != ' ':
+                in_cpu_info = False
+        return nr_running, nr_uninterruptible
+
+
+def get_running_task(with_key_line=True):
+    regex = re.compile('\s+')
+    with open(SD_PATH) as f:
+        cur_cpu = None
+        in_task_list = 0
+        running_tasks = {}
+        key_line = ''
+        for line in f.readlines():
+            if line.startswith('cpu#'):
+                cur_cpu = line[4:line.find(',')]
+                running_tasks[cur_cpu] = []
+            elif line.startswith('runnable tasks:'):
+                in_task_list = 1
+            elif in_task_list == 1:
+                if len(key_line) == 0:
+                    # keys = list(regex.split(line.strip()))
+                    key_line = line.rstrip()
+                if line[0] == '-':
+                    in_task_list = 2
+            elif in_task_list == 2:
+                if len(line) < 2 or line[1] == ' ':
+                    in_task_list = 0
+                    continue
+                # content = regex.split(line.strip())
+                # if content[0][-1] == 'R':
+                #     running_tasks[cur_cpu].append(content)
+                if line[1] == 'R':
+                    running_tasks[cur_cpu].append(line.rstrip())
+        if with_key_line:
+            return running_tasks, key_line
+        return running_tasks
+
+
+def monitor_loadavg_param(interval=1.0):
+    while True:
+        print(time.time(), get_loadavg_param(), flush=True)
+        time.sleep(interval)
+
+
+def print_running_task():
+    running_tasks, key_line = get_running_task(with_key_line=True)
+    for cpu_id, tasks in running_tasks.items():
+        if len(tasks) == 0:
+            continue
+        print('='*120)
+        print(f'cpu#{cpu_id}')
+        print(key_line)
+        print('-'*120)
+        for task in tasks:
+            print(task)
+        print(flush=True)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage:', sys.argv[0], 'loadavg_param | task_list', file=sys.stderr)
+        sys.exit(1)
+    if sys.argv[1] == 'loadavg_param':
+        if len(sys.argv) == 3:
+            interval = float(sys.argv[2])
+            monitor_loadavg_param(interval)
+        else:
+            monitor_loadavg_param()
+    elif sys.argv[1] == 'task_list':
+        print_running_task()
+    else:
+        print('usage:', sys.argv[0], 'loadavg_param | task_list', file=sys.stderr)
+        sys.exit(1)
