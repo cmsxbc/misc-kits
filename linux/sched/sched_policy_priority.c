c05896804d7a8a559b91085d66c90fdf8e6f67f5
cmsxbc <i@cmsis.me>
2021-05-19 23:02:47 +0800

    sched_policy_priority: add SCHED_DEADLINE

--- linux/sched/sched_policy_priority.c
+++ linux/sched/sched_policy_priority.c
@@ -6,28 +6,108 @@
 #include <time.h>
 #include <sched.h>
 #include <threads.h>
+#include <getopt.h>
+#include <syscall.h>
+#include <pthread.h>
 
 
 #define EXIT_API_ERROR 1
-#define EXIT_LOGICAL 2
+#define EXIT_PARAM_ERROR 2
+#define EXIT_LOGICAL_ERROR 3
 
-#define exit_error(errno, err) do \
+#define exit_perror(errno, err) do \
 { \
     perror(err); \
     exit(errno); \
 } while (0);
 
-#define exit_api_error(err) exit_error(EXIT_API_ERROR, err)
+#define exit_fprintf(errno, fd, ...) do \
+{                                  \
+    fprintf(stderr, __VA_ARGS__);  \
+    exit(errno);                   \
+} while (0);
+
+#define exit_api_error(err) exit_perror(EXIT_API_ERROR, err)
+#define exit_param_error(...) exit_fprintf(EXIT_PARAM_ERROR, stderr, __VA_ARGS__)
+#define exit_logic_error(...) exit_fprintf(EXIT_LOGICAL_ERROR, stderr, __VA_ARGS__)
 
 typedef long long nsec_t;
 
 typedef nsec_t heap_ele_t;
 typedef nsec_t *heap_t;
 
+
+static inline const char * get_policy_name(int policy) {
+    switch (policy) {
+        case SCHED_OTHER:
+            return "SCHED_NORMAL";
+        case SCHED_BATCH:
+            return "SCHED_BATCH";
+        case SCHED_RR:
+            return "SCHED_RR";
+        case SCHED_FIFO:
+            return "SCHED_FIFO";
+        case SCHED_DEADLINE:
+            return "SCHED_DEADLINE";
+        case SCHED_IDLE:
+            return "SCHED_IDLE";
+        default:
+            return "SCHED_UNKNOWN";
+    }
+}
+
+
+struct sched_attr {
+    u_int32_t size;
+    u_int32_t policy;
+    u_int64_t flags;
+    int32_t nice;
+    u_int32_t priority;
+    u_int64_t runtime;
+    u_int64_t deadline;
+    u_int64_t period;
+};
+
 void set_scheduler(int policy, const struct sched_param *param) {
     pid_t tid = gettid();
     if (-1 == sched_setscheduler(tid, policy, param)) {
         exit_api_error("set scheduler policy failed");
+    }
+}
+
+void set_default_sched_attr(int policy, struct sched_attr *attr) {
+    attr->size = sizeof(struct sched_attr);
+    attr->policy = policy;
+    attr->flags = 0;
+    switch (policy) {
+        case SCHED_OTHER:
+        case SCHED_BATCH:
+            attr->nice = 0;
+            break;
+        case SCHED_FIFO:
+        case SCHED_RR:
+            attr->priority = 1;
+            break;
+        case SCHED_IDLE:
+            attr->nice = 0;
+            attr->priority = 0;
+            break;
+        case SCHED_DEADLINE:
+            attr->runtime = 1000000000;
+            attr->deadline = 1000000000;
+            attr->period = 1000000000;
+            break;
+        default:
+            exit_logic_error("unknown supported policy: %s(%d)", get_policy_name(policy), policy);
+    }
+}
+
+void set_scheduler_attr(const struct sched_attr *attr) {
+    pid_t tid = gettid();
+    long ret = syscall(__NR_sched_setattr, tid, attr, 0);
+    // printf("syscall return: %d, %ld\n", attr->policy, ret);
+    if (ret == -1) {
+        exit_api_error("set scheduler attr failed");
     }
 }
 
@@ -125,7 +205,7 @@ static struct result_queue result_end_msg;
 
 struct thread_args {
     int policy;
-    struct sched_param param;
+    struct sched_attr attr;
     nsec_t sleep_time_ns;
     long interval;
     long total_times;
@@ -136,7 +216,7 @@ struct thread_args {
 
 void set_default_thead_args(struct thread_args *args, struct result_queue *result_head) {
     args->policy = SCHED_OTHER;
-    args->param.sched_priority = 0;
+    set_default_sched_attr(args->policy, &args->attr);
     args->sleep_time_ns = 20000000; // 20 ms;
     args->interval = 1000000000; // 1s;
     args->total_times = 50;
@@ -146,7 +226,7 @@ void set_default_thead_args(struct thread_args *args, struct result_queue *resul
 
 int thread_func(void * args) {
     struct thread_args *targs = (struct thread_args*) args;
-    set_scheduler(targs->policy, &targs->param);
+    set_scheduler_attr(&targs->attr);
     nsec_t sleep_time_ns = targs->sleep_time_ns;
     useconds_t sleep_time = sleep_time_ns / 1000;
     long interval = targs->interval;
@@ -156,6 +236,7 @@ int thread_func(void * args) {
     heap_t heap = create_heap(top10n);
     // printf("times, mean, [min, %ldth, max]\n", top10n);
     targs->tid = gettid();
+    pthread_setname_np(pthread_self(), get_policy_name(targs->policy));
     while (total_times -- > 0) {
         nsec_t s = 0, e = 0, d = 0;
         nsec_t total_diff = 0, max_diff = 0, min_diff = (1ULL << 63) - 1;
@@ -190,53 +271,12 @@ int thread_func(void * args) {
     return thrd_success;
 }
 
-static inline const char * get_policy_name(int policy) {
-    switch (policy) {
-        case SCHED_OTHER:
-            return "SCHED_NORMAL";
-        case SCHED_BATCH:
-            return "SCHED_BATCH";
-        case SCHED_RR:
-            return "SCHED_RR";
-        case SCHED_FIFO:
-            return "SCHED_FIFO";
-        case SCHED_DEADLINE:
-            return "SCHED_DEADLINE";
-        case SCHED_IDLE:
-            return "SCHED_IDLE";
-        default:
-            return "SCHED_UNKNOWN";
-    }
-}
 
-int main(int argc, char const *argv[]) {
-    if (argc <= 1) {
-        printf("usage: %s policy1 [policy2 [policy3 ...]]", argv[0]);
-        exit(2);
-    }
-    struct sched_param param = {0};
-    set_scheduler(SCHED_OTHER, &param);
-    int thread_count = argc - 1;
-    struct thread_args *args = calloc(thread_count, sizeof(struct thread_args));
-    for (int i = 0; i < thread_count; i++) {
-        set_default_thead_args(&args[i], malloc(sizeof(struct result_queue)));
-        if (strcmp(argv[i+1], "IDLE") == 0) {
-            args[i].policy = SCHED_IDLE;
-        } else if (strcmp(argv[i+1], "FIFO") == 0) {
-            args[i].policy = SCHED_FIFO;
-            args[i].param.sched_priority = 1;
-        } else if (strcmp(argv[i+1], "RR") == 0) {
-            args[i].policy = SCHED_RR;
-            args[i].param.sched_priority = 1;
-        } else if (strcmp(argv[i+1], "NORMAL") == 0) {
-            // although it's default;
-            args[i].policy = SCHED_OTHER;
-        } else {
-            printf("unknown scheduler policy: %s", argv[i+1]);
-            exit(EXIT_LOGICAL);
-        }
-    }
+void do_mt(int thread_count, struct thread_args *args) {
     thrd_t *threads = calloc(thread_count, sizeof(thrd_t));
+    if (threads == NULL) {
+        exit_api_error("alloc threads failed");
+    }
     for (int i = 0; i < thread_count; i++) {
         printf("create thread %d with policy %s\n", i, get_policy_name(args[i].policy));
         if (thrd_create(&threads[i], thread_func, &args[i]) != thrd_success) {
@@ -257,7 +297,8 @@ int main(int argc, char const *argv[]) {
             continue;
         }
         printf("pid: %d\n", pid);
-        printf("  threads,          policy,  timestamp,      mean, [      min,       P90,      max]\n");
+        // printf("  threads,          policy,  timestamp,      mean, [      min,       P90,      max]\n");
+        printf("%3s(%7s), %14s, %10s, %9s, [%-9s, %9s, %9s]\n", "no", "tid", "policy", "timestamp", "mean", "min", "p90", "max");
         for (int i = 0; i < thread_count; i ++) {
             struct result_queue *result = args[i].result_head->next;
             if (result == NULL) {
@@ -267,7 +308,7 @@ int main(int argc, char const *argv[]) {
                 printf("thread %d finished!\n", i);
                 nr_running --;
             } else {
-                printf("%d(%7d), %14s, %10ld, %9lld, [%9lld, %9lld, %9lld]\n", i, args[i].tid,
+                printf("%3d(%7d), %14s, %10ld, %9lld, [%-9lld, %9lld, %9lld]\n", i, args[i].tid,
                        get_policy_name(args[i].policy), result->timestamp,
                        result->mean_diff, result->min_diff, result->top10n_diff, result->max_diff);
             }
@@ -280,6 +321,35 @@ int main(int argc, char const *argv[]) {
         thrd_join(threads[i], NULL);
     }
     free(threads);
+}
+
+int main(int argc, char const *argv[]) {
+    if (argc <= 1) {
+        exit_param_error("usage: %s policy1 [policy2 [policy3 ...]]", argv[0]);
+    }
+    struct sched_param param = {0};
+    set_scheduler(SCHED_OTHER, &param);
+    int thread_count = argc - 1;
+    struct thread_args *args = calloc(thread_count, sizeof(struct thread_args));
+    for (int i = 0; i < thread_count; i++) {
+        set_default_thead_args(&args[i], malloc(sizeof(struct result_queue)));
+        if (strcmp(argv[i+1], "IDLE") == 0) {
+            args[i].policy = SCHED_IDLE;
+        } else if (strcmp(argv[i+1], "FIFO") == 0) {
+            args[i].policy = SCHED_FIFO;
+        } else if (strcmp(argv[i+1], "RR") == 0) {
+            args[i].policy = SCHED_RR;
+        } else if (strcmp(argv[i+1], "NORMAL") == 0) {
+            // although it's default;
+            args[i].policy = SCHED_OTHER;
+        } else if (strcmp(argv[i+1], "DEADLINE") == 0) {
+            args[i].policy = SCHED_DEADLINE;
+        } else {
+            exit_param_error("unknown scheduler policy: %s", argv[i+1]);
+        }
+        set_default_sched_attr(args[i].policy, &args[i].attr);
+    }
+    do_mt(thread_count, args);
     free(args);
     return 0;
 }
