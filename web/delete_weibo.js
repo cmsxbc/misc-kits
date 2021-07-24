e4276be1398acc93ec2efe84f88e529ad5f65ffc
cmsxbc <i@cmsis.me>
2021-07-24 17:24:50 +0800

    update

--- web/delete_weibo.js
+++ web/delete_weibo.js
@@ -1,7 +1,15 @@
-function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), reversed=false) {
+/*
+Weibo is sooooooooo terrible!
+*/
+
+function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), reversed=false, offset=0) {
     function Forget(cb, interval) {
         var allWeiboInPage = document.querySelector('div.vue-recycle-scroller__item-wrapper').children,
-            idx = reversed ? allWeiboInPage.length - 1: 0;
+            idx = reversed ? allWeiboInPage.length - 1 - offset: offset;
+        if (idx > allWeiboInPage.length - 1 || idx < 0) {
+            setTimeout(cb, interval);
+            return;
+        }
         allWeiboInPage[idx].querySelector(`i.morepop_action_${class_suffix}`).click();
         setTimeout(
             () => {
@@ -40,7 +48,7 @@ function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), 
     Loop();
 }
 
-function IWannaForgetAll(class_suffix) {
+function IWannaForgetAll(class_suffix, offset=0) {
     const ItemsPreLoop = 10;
     function IWishYouCanBeHappy() {
         window.scrollTo(0, ItemsPreLoop * 1000 * 2);
@@ -48,10 +56,10 @@ function IWannaForgetAll(class_suffix) {
             document.querySelector('i.woo-font--backTop').parentElement.parentElement.click()
             // window.scroll(0, 100);
             // document.querySelector('div.vue-recycle-scroller__item-wrapper').children[0].focus();
-            setTimeout(() => IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy, true), 1000);
+            setTimeout(() => IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy, true, offset), 1000);
         }, 10000);
     }
-    IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy);
+    IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy, false, offset);
     // IWishYouCanBeHappy();
 }
 
