a5e114cedbcfe70d2e9155cdf69b9abd8314a6a7
cmsxbc <i@cmsis.me>
2023-07-22 13:07:33 +0800

    fix(web): update delete_weibo for new ui

--- web/delete_weibo.js
+++ web/delete_weibo.js
@@ -2,7 +2,7 @@
 Weibo is sooooooooo terrible!
 */
 
-function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), reversed=false, offset=0) {
+function IWannaForget(class_suffix, root_suffix, limit=10, fcb=()=>console.log('finished!'), reversed=false, offset=0) {
     function Forget(cb, interval) {
         var allWeiboInPage = document.querySelector('div.vue-recycle-scroller__item-wrapper')?.children;
         if (!allWeiboInPage) {
@@ -18,7 +18,7 @@ function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), 
         setTimeout(
             () => {
                 var hasDelete = false;
-                for (var child of document.querySelector('div.woo-pop-wrap-main').children) {
+                for (var child of document.querySelector(`.morepop_more_${root_suffix} div.woo-pop-wrap-main`).children) {
                     if (child.innerText == '\u5220\u9664') {
                         child.click();
                         hasDelete = true;
@@ -53,7 +53,7 @@ function IWannaForget(class_suffix, limit=10, fcb=()=>console.log('finished!'), 
     Loop();
 }
 
-function IWannaForgetAll(class_suffix, offset=0) {
+function IWannaForgetAll(class_suffix, root_suffix, offset=0) {
     const ItemsPreLoop = 10;
     function IWishYouCanBeHappy() {
         window.scrollTo(0, ItemsPreLoop * 1000 * 2);
@@ -61,30 +61,41 @@ function IWannaForgetAll(class_suffix, offset=0) {
             document.querySelector('i.woo-font--backTop').parentElement.parentElement.click()
             window.scroll(0, 100);
             document.querySelector('div.vue-recycle-scroller__item-wrapper').children[0].focus();
-            setTimeout(() => IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy, true, offset), 1000);
+            setTimeout(() => IWannaForget(class_suffix, root_suffix, ItemsPreLoop, IWishYouCanBeHappy, true, offset), 1000);
         }, 10000);
     }
-    IWannaForget(class_suffix, ItemsPreLoop, IWishYouCanBeHappy, false, offset);
+    IWannaForget(class_suffix, root_suffix, ItemsPreLoop, IWishYouCanBeHappy, false, offset);
 }
 
-(function IWillHelpYouForgetAll() {
-    let suffixes = {}
-    for (let action_btn of document.querySelectorAll("i.woo-font--angleDown")) {
-        let classList = Array.from(action_btn.classList);
-        for (let className of classList) {
-            if (!className.startsWith("morepop_action_")) {
-                continue;
-            }
-            let suffix = className.replace("morepop_action_", "");
-            if (suffix in suffixes) {
-                suffixes[suffix] += 1;
-            } else {
-                suffixes[suffix] = 1;
+(() => {
+    function GetSuffixes(selector, prefix) {
+        let suffixes = {}
+        for (let action_btn of document.querySelectorAll(selector)) {
+            let classList = Array.from(action_btn.classList);
+            for (let className of classList) {
+                if (!className.startsWith(prefix)) {
+                    continue;
+                }
+                let suffix = className.replace(prefix, "");
+                if (suffix in suffixes) {
+                    suffixes[suffix] += 1;
+                } else {
+                    suffixes[suffix] = 1;
+                }
             }
         }
+        return suffixes;
     }
+
+    let suffixes = GetSuffixes("i.woo-font--angleDown", "morepop_action_");
     let sorted_suffixes = Object.keys(suffixes).sort((a, b) => suffixes[b] - suffixes[a]);
-    console.log(suffixes, sorted_suffixes);
-    IWannaForgetAll(sorted_suffixes[0]);
+    console.log(suffixes);
+    console.log(sorted_suffixes);
+    console.log(sorted_suffixes[0]);
+    let root_suffixes = GetSuffixes(`.woo-pop-wrap:has(.morepop_action_${sorted_suffixes[0]})`, "morepop_more_");
+    console.log(root_suffixes)
+    let root_suffix = Object.keys(root_suffixes).sort((a, b) => root_suffixes[b] - root_suffixes[a])[0];
+    console.log(root_suffix);
+    IWannaForgetAll(sorted_suffixes[0], root_suffix);
 })();
 
